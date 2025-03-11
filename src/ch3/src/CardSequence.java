498afb69efbd3f852136a01dc001ee8e665ddfb2
François Pagé <francois.page@mail.mcgill.ca>
2025-03-11 18:13:11 -0400

    Start of chapter 3 exercise

---
+++ src/ch3/src/CardSequence.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class CardSequence implements CardSource {
+    private List<Card> cards;
+
+    public CardSequence() {
+        // ...
+    }
+
+    public void addCard(Card card) {
+        // ...
+    }
+
+    public List<Card> getCards() {
+        // ...
+    }
+}
